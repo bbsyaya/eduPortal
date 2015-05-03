4721534a3b580b0f9b3c259716f1685fd47b2691
wanjinma <13774266242@hotmail.com>
2015-05-03 23:04:58 +0800

    Initial commit

---
+++ src/com/milk/eduPortal/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.milk.eduPortal;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                try {
+                    sleep(2000);
+                    Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+    }
+
+}
