00eaf47f78b11281800e2a2133f519092b5165fc
wanjinma <13774266242@hotmail.com>
2015-08-06 00:37:42 +0800

    返回问题

---
+++ assets/emobile/js/ctrl/aboutus_ctrl.js
@@ -0,0 +1,46 @@
+// 
+// Here is how to define your module 
+// has dependent on mobile-angular-ui
+// 
+var app = angular.module('emobile', [
+    'ngRoute',
+    'mobile-angular-ui',
+    'mobile-angular-ui.gestures'
+]);
+
+app.filter('trustHtml', function ($sce) {
+    return function (input) {
+        return $sce.trustAsHtml(input);
+    }
+});
+
+app.config(function ($routeProvider,$httpProvider) {
+    $routeProvider.when('/', {templateUrl: 'aboutus.html', controller: 'mainController', reloadOnSearch: false});
+    $routeProvider.when('/setting_intro', {templateUrl: 'setting_intro.html', controller: 'mainController', reloadOnSearch: false});
+    $routeProvider.when('/setting_service', {templateUrl: 'setting_service.html', controller: 'mainController', reloadOnSearch: false});
+    $routeProvider.when('/setting_manager', {templateUrl: 'setting_manager.html', controller: 'mainController', reloadOnSearch: false});
+    $routeProvider.when('/setting_guard', {templateUrl: 'setting_guard.html', controller: 'mainController', reloadOnSearch: false});
+
+    $httpProvider.defaults.transformRequest = function(data){
+        if (data === undefined) {
+            return data;
+        }
+        return $.param(data);
+    }
+    $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8';
+});
+
+app.controller('mainController', function ($rootScope, $scope, $http,$location) {
+
+
+    $scope.back = function () {
+        window.history.go(-1);
+    };
+
+
+
+
+});
+
+
+
