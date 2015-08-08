0898850847f061b933b7f1661e50496a6dbf7132
wanjinma <13774266242@hotmail.com>
2015-08-08 15:17:34 +0800

    gps问题

--- assets/emobile/js/ctrl/aboutus_ctrl.js
+++ assets/emobile/js/ctrl/aboutus_ctrl.js
@@ -3,9 +3,9 @@
 // has dependent on mobile-angular-ui
 // 
 var app = angular.module('emobile', [
-    'ngRoute',
-    'mobile-angular-ui',
-    'mobile-angular-ui.gestures'
+    'ngRoute'
+    //'mobile-angular-ui',
+    //'mobile-angular-ui.gestures'
 ]);
 
 app.filter('trustHtml', function ($sce) {
@@ -17,10 +17,10 @@ app.filter('trustHtml', function ($sce) {
 app.config(function ($routeProvider,$httpProvider) {
     $routeProvider.when('/', {templateUrl: 'aboutus.html', controller: 'mainController', reloadOnSearch: false})
 
-    .when('/setting_intro', {templateUrl: 'setting_intro.html', controller: 'mainController', reloadOnSearch: false})
+    .when('/setting_intro', {templateUrl: 'setting_intro.html', controller: 'introController', reloadOnSearch: false})
 
-        .when('/setting_service11', {templateUrl: 'setting_services.html', controller: 'mainController', reloadOnSearch: false})
-
+        .when('/setting_service11', {templateUrl: 'setting_service1s.html', controller: 'serviceController', reloadOnSearch: false})
+        //.when('/setting_service11', {templateUrl: 'setting_service1s.html', controller: 'mainController', reloadOnSearch: false})
         .when('/setting_manager', {templateUrl: 'setting_manager.html', controller: 'mainController', reloadOnSearch: false})
 
         .when('/setting_guard', {templateUrl: 'setting_guard.html', controller: 'mainController', reloadOnSearch: false})
@@ -37,15 +37,27 @@ app.config(function ($routeProvider,$httpProvider) {
     $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8';
 });
 
-app.controller('mainController', function ($rootScope, $scope, $http,$location) {
-
+app.controller('mainController', function ($rootScope, $scope) {
 
     $scope.back = function () {
         window.history.go(-1);
     };
 
+});
 
+app.controller('introController', function ($rootScope, $scope) {
 
+    $scope.back = function () {
+        window.history.go(-1);
+    };
+
+});
+
+app.controller('serviceController', function ($rootScope, $scope) {
+
+    $scope.back = function () {
+        window.history.go(-1);
+    };
 
 });
 
